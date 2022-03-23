12a2c975f7222cd143f56608d2791424bcd3a0c9
CheyenneKoh <60183723+CheyenneKoh@users.noreply.github.com>
2022-03-23 15:30:17 +0800

    removed console log

--- app/redux/reducer.js
+++ app/redux/reducer.js
@@ -3,7 +3,6 @@ import * as actions from "./actionTypes";
 export default (state = [], action) => {
   switch (action.type) {
     case actions.RETRIEVE_RESTAURANTS:
-      console.log(action.payload);
       return [...state].concat(action.payload);
 
     case actions.STAR_LOCATION:
